a79a7d565a15abae5066fe28c3ec245710a9201b
tsolid <6312338+tsolid@users.noreply.github.com>
2020-12-03 17:43:09 -0500

    first

---
+++ devicetypes/tsolid/lacrosse-temp-probe.src/lacrosse-temp-probe.groovy
@@ -0,0 +1,125 @@
+/**
+ *  The Weather Company Web
+ *
+ *  Copyright 2020 tsolid
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+metadata {
+	definition (name: "Lacrosse Temp Probe", namespace: "tsolid", author: "Tom Sxxxx") {
+		capability "refresh"
+        capability "polling"
+        capability "sensor"
+        capability "capability.temperatureMeasurement"
+        capability "capability.relativeHumidityMeasurement"
+	}
+
+	tiles(scale: 2) {
+
+	standardTile("LT_Probe", "device.LT_Probe",  width: 6, height: 3,  canChangeIcon: false ) {
+            state "default", icon: "https://cdn.shopify.com/s/files/1/0035/7443/1790/files/Main_copy_2x_f85b234b-d84f-43ea-91b3-92cc0f5ebaaf_190x.png", backgroundColor: "#999999"      } 
+
+	 standardTile("temperature", "device.temperature", width: 2, height: 2, canChangeIcon: false) {
+            state "default", label: '${currentValue}º',unit:'${currentValue}', icon: "st.Weather.weather2", backgroundColor:"#999999"}  
+        
+	standardTile("humidity", "device.humidity", width: 2, height: 2, canChangeIcon: false) {
+            state "default", label: '${currentValue}%', icon: "st.Weather.weather12", backgroundColor:"#999999"      }
+
+	standardTile("refresh", "device.refresh", decoration: "flat", width: 2, height: 2) {
+ 		state "default", action:"refresh", icon:"st.secondary.refresh"
+ 		} 
+	
+	main("temperature")
+	details(["LT_Probe","temperature","humidity","refresh" ])
+ 	}
+}
+
+def installed() {
+    
+
+}
+
+def updated() {
+
+	log.debug "Executing 'updated'"
+	refresh()
+}
+
+def poll(){
+	log.debug "Executing 'poll'"
+	refresh()
+}
+
+// parse events into attributes
+def parse(String description) {
+
+	log.debug "Executing 'parse'"
+	state.lowtempalert=false
+	refresh()    
+	runEvery10Minutes(forcepoll)
+    
+}
+
+def forcepoll()
+{
+	refresh()
+}
+
+// handle commands
+def refresh() {
+	log.debug "Executing 'refresh'"
+
+	def mymap = makeJSONTempRequest() 
+	log.debug mymap
+
+	log.debug "response relative_humidity: ${mymap['humidity']}"
+	log.debug "response temp_f: ${mymap['temp']}"
+
+	sendEvent(name: "humidity", value:  mymap['humidity'])
+	sendEvent(name: "temperature", value: mymap['temp'], unit: temperatureScale)
+
+	if (getDataValue("LTPlowtempalert")) {
+		if (getDataValue("LTPlowtempalert").toFloat() >= mymap['temperature'].toFloat())
+		{
+			if ( state.lowtempalert == false) {
+                		sendEvent(name:"Alert", value: "Probe Low Temperature Alert!", displayed:true)
+				state.lowtempalert=true 
+			}
+        	}
+	        else
+	            state.lowtempalert=false
+	}
+
+}
+
+def makeJSONTempRequest() {
+    def params = [
+        uri:  'http://decent-destiny-704.appspot.com',
+        path: '/laxservices/device_info.php',
+        contentType: 'application/json',
+        query: [deviceid:'00012309F88805DA', limit: '5', timezone: '3', metric: '0']
+    ]
+
+	def result = [temp: "", humidity: ""]
+
+    try {
+        httpGet(params) {resp ->
+            log.debug "resp data: ${resp.data}"
+		result["temp"] = resp.data.device0.obs[0].probe_temp
+		result["humidity"] = resp.data.device0.obs[0].humidity
+        }
+    } catch (e) {
+        log.error "error: $e"
+    }
+
+    return result
+}
